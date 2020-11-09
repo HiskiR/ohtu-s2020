c38ced67b0547cc99a05726c943a851934d6eae2
HiskiR <hiski.r@gmail.com>
2020-11-09 17:31:11 +0200

    t9-12

---
+++ viikko2/tehtavat7-12/Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Viitegeneraattori.java
@@ -0,0 +1,14 @@
+package ohtu.verkkokauppa;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Viitegeneraattori implements ViitegeneraattoriInterface {
+    
+    private int seuraava = 1;
+    
+    @Override
+    public int uusi(){
+        return seuraava++;
+    }
+}
